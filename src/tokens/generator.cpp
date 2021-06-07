ecded70ce41c776e5eb98c8dfa962135b05d4a55
brian.orwe <brian.orwe@gmail.com>
2021-06-07 15:25:35 +0300

    Make safaricon_generator* connect

--- src/tokens/generator.cpp
+++ src/tokens/generator.cpp
@@ -1,7 +1,21 @@
+#include "boost/asio/deadline_timer.hpp"
+#include "boost/asio/io_context.hpp"
+#include "boost/asio/post.hpp"
+#include "boost/asio/ssl/context.hpp"
+#include "boost/asio/ssl/verify_mode.hpp"
+#include "boost/asio/steady_timer.hpp"
+#include "boost/beast/core/bind_handler.hpp"
+#include "boost/beast/core/error.hpp"
+#include "boost/beast/core/stream_traits.hpp"
+#include "boost/beast/core/tcp_stream.hpp"
+#include "boost/date_time/posix_time/posix_time_duration.hpp"
+#include "boost/system/error_code.hpp"
 #include <boost/asio.hpp>
 #include <boost/utility/string_view_fwd.hpp>
+#include <chrono>
 #include <daraja/tokens/generator.hpp>
 #include <boost/beast.hpp>
+#include <boost/beast/ssl.hpp>
 #include <iostream>
 #include <memory>
 
@@ -15,58 +29,53 @@ namespace Daraja{
 
         class safaricom_tokens_getter:
             public std::enable_shared_from_this<safaricom_tokens_getter>{
+                std::shared_ptr<net::ssl::context> m_ctx;
                 tcp::resolver m_resolver;
-                beast::tcp_stream m_stream;
+                beast::ssl_stream<beast::tcp_stream> m_stream;
                 beast::flat_buffer m_buffer;
                 http::request<http::empty_body> m_request;
                 http::response<http::string_body> m_response;
                 const std::string m_server_query_endpoint;
                 const std::string m_key;
                 const std::string m_secret;
+                std::string m_address;
             public:
 
 
                 
                 explicit safaricom_tokens_getter(net::io_context &io
+                        ,std::shared_ptr<net::ssl::context> ctx
                         ,const ConsumerValues &conf)
-                    :m_resolver(net::make_strand(io)),
-                    m_stream(net::make_strand(io)),m_key(conf.getKey()),
-                    m_secret(conf.getSecret()),m_server_query_endpoint(conf.getEndpoint()){
-                }
+                    :m_ctx(ctx),
+                    m_resolver(net::make_strand(io)),
+                    m_stream(net::make_strand(io),*m_ctx), 
+                    m_server_query_endpoint(conf.getEndpoint()),
+                    m_key(conf.getKey()),
+                    m_secret(conf.getSecret()){}
 
                 void run(){
                     //get url from endpoint
-                    bool hasHttp =
-                        boost::string_view(m_server_query_endpoint)
-                            .starts_with("htpp://");
-                    bool hasHttps =
-                        boost::string_view(m_server_query_endpoint)
-                            .starts_with("htpps://");
-                    boost::string_view host="";
-                    if(hasHttp || hasHttps){
-                        host=m_server_query_endpoint.substr(6);
-                    }else if(hasHttps){
-                        host=m_server_query_endpoint.substr(7);
-                    }else{
-                        host=m_server_query_endpoint;
-                    }
+                    std::string host=m_server_query_endpoint.substr(8);
                     int end_of_host=host.find_first_of("/");
                     auto tmp=host;
                     host=host.substr(0,end_of_host);//host address
                     auto query=tmp.substr(end_of_host);// beginning of query
-                    std::string host_to_use="localhost";
 
+                    m_address = host;
 
                     int http_version=11;
-                    std::string port="80";
+                    std::string port="443";
+
+                    //m_address="google.com";
+                    std::cout<<m_address<<"\n";
 
                     //setup the request object
                     m_request.version(http_version);
                     m_request.method(http::verb::get);
                     m_request.target(query);
-                    m_request.set(http::field::host,host);
+                    m_request.set(http::field::host,m_address);
 
-                    m_resolver.async_resolve(host_to_use,port,
+                    m_resolver.async_resolve(m_address,port,
                             beast::bind_front_handler(
                                 &safaricom_tokens_getter::on_resolve,
                                 shared_from_this()));
@@ -75,15 +84,47 @@ namespace Daraja{
                 void on_resolve(beast::error_code ec,
                         tcp::resolver::results_type results){
                     if(ec){
-                        std::cout<<"COME ON FUCK"<<std::endl;
+                        fail(ec,"On resolve");
+                        return;
                     }
-                    std::cout<<"YEAHHHH: "<<std::endl;
+                    m_stream.next_layer()
+                        .expires_after(std::chrono::seconds(5));
+                    m_stream.next_layer().async_connect(results,
+                            beast::bind_front_handler( 
+                                &safaricom_tokens_getter::on_connect,
+                                shared_from_this()));
+                }
+
+                void on_connect(beast::error_code ec,
+                        tcp::resolver::results_type::endpoint_type endpoint_type){
+                    if (ec){
+                        fail(ec,"On connect");
+                        //try reconnect after 5 seconds
+                        net::deadline_timer t(
+                                m_stream.get_executor(), boost::posix_time::seconds(1));
+                        t.async_wait([self=shared_from_this()]
+                                (boost::system::error_code ec){
+                                self->m_resolver.async_resolve(self->m_address,"443",
+                                        beast::bind_front_handler(
+                                            &safaricom_tokens_getter::on_resolve,
+                                            self->shared_from_this()));
+                        });
+                        return;
+                    }
+                    std::cout<<"YEAH!!!!!!\n";
+                }
+
+                /***
+                 * Print error message
+                 */
+                void fail(beast::error_code &code, const char *err){
+                    std::cerr << err << ": "<< code.message() << std::endl;
                 }
         };
 
         AccessGenerator::AccessGenerator(const ConsumerValues &conf,
-                bool asyncGenerate):doAsync(asyncGenerate),
-            conf(conf){
+                bool asyncGenerate):
+            conf(conf),doAsync(asyncGenerate){
         }
 
         const std::string AccessGenerator::getAccessToken()const{
@@ -93,7 +134,11 @@ namespace Daraja{
         //TODO this is just for tests, clean this function to do appropriate thing
         void AccessGenerator::start()const{
             net::io_context ios;
-            std::make_shared<safaricom_tokens_getter>(ios,this->conf)->run();
+            auto ctx=
+                std::make_shared<net::ssl::context>(
+                        boost::asio::ssl::context::sslv23_client);
+            ctx->set_verify_mode(net::ssl::verify_none);
+            std::make_shared<safaricom_tokens_getter>(ios,ctx,this->conf)->run();
             ios.run();
         }
     }
--- tests/token_api.cpp
+++ tests/token_api.cpp
@@ -14,6 +14,7 @@
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "daraja/tokens/consumer_values.hpp"
+#include <iostream>
 #define BOOST_TEST_MODULE TOKEN_API
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/path.hpp>
@@ -53,7 +54,7 @@ BOOST_AUTO_TEST_CASE(create_access_token){
     const std::string access_token=access_async.getAccessToken();
     //sleep for 3 seconds and retry getting token which should be different
     const std::string access_token2=access_async.getAccessToken();
-    //BOOST_TEST(access_token.empty()==false);
+    BOOST_TEST(access_token.empty()==false);
     //BOOST_TEST(access_token2.empty()==false);
     //BOOST_TEST(access_token!=access_token2);
 }
